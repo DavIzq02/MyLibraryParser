72fb08fe092670bd47f032d4fd9cda081848a098
David Izquierdo <david.izq02@hotmail.com>
2026-09-19 20:18:09 -0500

    Main principal del proyecto se borra contenido , no se requiere ejecutar el proyecto directamente
    Main txt -> archivo que debe usarse como main principal en los proyecto que se desen escanear con la libreria

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,5 @@
 public class Main {
-            private static final String RUTA = "C:\\Users\\Admin\\Desktop\\Proyecto_Poo";
 
     public static void main(String[] args) {
-//
-//        long inicio = System.nanoTime();
-//
-//        JavaProjectParser projectParser = new JavaProjectParser(new AnalysisConfiguration(JavaLanguageVersion.JAVA_17));
-//
-//        UmlModel model = projectParser.parse(Path.of(RUTA));
-//
-//        UmlModelMarkdownExporter exporter = new UmlModelMarkdownExporter();
-//        exporter.export(model,Path.of("uml-model-output.md"));
-//        long fin = System.nanoTime();
-//        long tiempoNanosegundos =  fin - inicio;
-//
-//        double tiempoMilisegundos = tiempoNanosegundos / 1_000_000.0;
-//        System.out.println("Análisis terminado correctamente.");
-//
-//        System.out.println( "Archivo generado: uml-model-output.md");
-//
-//        System.out.println("Tiempo total de ejecución: "+ tiempoMilisegundos+ " ms" );
-
     }
 }
